3196895825403b7d7ecd870f734a69a4bffefe60
EC2 Default User <ec2-user@ip-172-31-30-2.us-east-2.compute.internal>
2018-03-20 07:47:25 +0000

    kadai

---
+++ test-carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
